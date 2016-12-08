8bef085377e8781b194132f49ac0541987820e54
Ray Minge <ray@rayminge.com>
2016-12-07 23:53:40 -0600

    Adds settings file

--- src/js/noir-app.js
+++ src/js/noir-app.js
@@ -1,7 +1,21 @@
 const NoirIrc = require('./js/Noir/NoirIrc/NoirIrc.js');
+const { remote } = require('electron');
 
-var client = new NoirIrc('irc.freenode.com', 'noirbot', {
-	userName: 'noirbot'
-}, [
-	'#raytestmn'
-]);
+
+remote.getGlobal('SETTINGS').connections
+	.filter(cxn => cxn.type == 'noir-contrib-irc' )
+	.forEach(cxn => {
+		var config = cxn.config || {};
+		if (config.channels) {
+			cxn.channels = config.channels;
+			delete config['channels'];
+		}
+		if (! config.userName) {
+			config.userName = cxn.userName;
+		}
+		if (! config.nick) {
+			config.nick = cxn.userName;
+		}
+		cxn.config = config;
+		new NoirIrc(cxn.host, cxn.name || cxn.host, cxn.userName, cxn.config, cxn.channels);
+	})
--- src/main.js
+++ src/main.js
@@ -1,11 +1,44 @@
 const {app, BrowserWindow, Menu, shell} = require('electron')
 const path = require('path')
 const url = require('url')
-
+const fs = require('fs');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
+process.umask = 0;
+
+global.DIR_APPDATA  = app.getPath('userData');
+global.DIR_SETTINGS = DIR_APPDATA + "/Settings";
+global.FILE_PREFS   = DIR_SETTINGS + "/prefs.json";
+
+if (! fs.existsSync(DIR_APPDATA)) {
+  fs.mkdirSync(DIR_APPDATA, '0777');
+}
+
+if (! fs.existsSync(DIR_SETTINGS)) {
+  fs.mkdirSync(DIR_SETTINGS, '0777');
+}
+
+if (! fs.existsSync(global.FILE_PREFS)) {
+  let defaults = {
+    "connections": [
+      {
+        "type": "noir-contrib-irc",
+        "name": "Thing 1",
+        "host": "irc.freenode.com",
+        "userName": "Thing1",
+        "channels": [
+          "#noirchat"
+        ]
+      }
+    ]
+  };
+  fs.writeFileSync(global.FILE_PREFS, JSON.stringify(defaults), { mode: '0777' });
+}
+
+var contents = fs.readFileSync(global.FILE_PREFS);
+global.SETTINGS = JSON.parse(contents);
 
 function createWindow () {
   // Create the browser window.
@@ -19,23 +52,35 @@ function createWindow () {
   }))
 
 
-  var template = [{
-      label: "Application",
-      submenu: [
-          { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-          { type: "separator" },
-          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-      ]}, {
-      label: "Edit",
-      submenu: [
-          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-          { type: "separator" },
-          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-      ]}
+  var template = [
+  {
+    label: "Application",
+    submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { label: "Preferences", submenu: [
+        {
+          label: "Settings",
+          accelerator: "CmdOrCtrl+,",
+          click: () => {
+            shell.openItem( global.FILE_PREFS, {}, function() {
+              throw new Error(JSON.stringify(Array.from(arguments)));
+            });
+          }
+        }
+      ]},
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
   ];
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
@@ -60,12 +105,14 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+// app.on('before-quit', () => {
+//   fs.writeFileSync(PREF_FILE, JSON.stringify(SETTINGS));
+// })
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
