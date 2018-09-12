eec5ec675c1c48645df45409285b0f15ab40fd63
Ray Minge <ray@rayminge.com>
2018-09-12 11:20:45 -0500

    Clerical nonsense

--- src/js/noir-app.js
+++ src/js/noir-app.js
@@ -4,16 +4,14 @@ const NoirYouTubeIframe = require( "./js/Noir/NoirYouTubeIframe/NoirYouTubeIfram
 const NoirLinkify = require( "./js/Noir/NoirLinkify/NoirLinkify.js" );
 const NoirYouTubeImagePreview = require( "./js/Noir/NoirYouTubeImagePreview/NoirYouTubeImagePreview.js" );
 const NoirMarked = require( "./js/Noir/NoirMarked/NoirMarked.js" );
+const NoirGiphy = require( "./js/Noir/NoirGiphy/NoirGiphy.js" );
 const NoirAutoComplete = require( "./js/Noir/NoirAutoComplete/NoirAutoComplete.js" );
 const NoirEmojione = require( "./js/Noir/NoirEmojione/NoirEmojione.js" );
 
 const { ChatAreaFactory } = require( "./js/Noir/Noir/ChatArea.js" );
 const { remote }     = require( "electron" );
 const Tabset         = require( "./js/Noir/Noir/Tabset.js" );
-// const marked         = require( "marked" );
-// const emojione       = require( "emojione" );
 
-// let emojis = Object.keys( emojione.emojioneList );
 let tabset = new Tabset();
 let chatAreaFactory = new ChatAreaFactory;
 let config = remote.getGlobal( "SETTINGS" );
@@ -24,13 +22,13 @@ let availablePlugins = {
 	NoirOgImagePreview,
 	NoirYouTubeIframe,
 	NoirMarked,
+	NoirGiphy,
 	NoirAutoComplete,
 	NoirEmojione
 };
 
 let plugins = Object.keys( config.plugins ).reduce( ( agg, key ) => {
 	try {
-		console.log( "KEY", key );
 		agg[ key ] = new ( availablePlugins[ key ] )( config.plugins[ key ] );
 	} catch ( e ) {
 		console.error( "ERROR registering plugin: ", e );
@@ -57,46 +55,6 @@ if ( config.autoCompletePlugins ) {
 		}
 	});
 }
-
-// chatAreaFactory.addAutoCompleteListener( function ircAutoComplete( e ) {
-
-// 	if ( ! e.word.match( /^:.+$/ ) ) {
-// 		return;
-// 	}
-
-// 	emojis
-// 		.filter( em => {
-// 			return em.slice( 0, e.word.length ) == e.word;
-// 		})
-// 		.forEach( em => {
-// 			let unicode = emojione.shortnameToUnicode( em );
-// 			e.autoCompleteTooltip.suggest(
-// 				unicode,
-// 				unicode + " " + em,
-// 				em.length
-// 			);
-// 		});
-// });
-
-// if ( config.commands ) {
-// 	let waitForChars = ( config.commandPromptAfter || 3 );
-// 	chatAreaFactory.addAutoCompleteListener( function ircAutoComplete( e ) {
-// 		Object.keys( config.commands ).forEach( shortName => {
-// 			let cmd = config.commands[shortName];
-// 			if ( e.word.length < waitForChars ) {
-// 				return;
-// 			}
-// 			if ( shortName.slice( 0, e.word.length ) != e.word ) {
-// 				return;
-// 			}
-// 			e.autoCompleteTooltip.suggest(
-// 				cmd,
-// 				shortName,
-// 				shortName.length
-// 			);
-// 		});
-// 	});
-// }
 
 ( function() {
 	var grabber = document.getElementById( "sidebarGrab" );
@@ -136,9 +94,16 @@ document.getElementById( "main" ).appendChild( tabset.view.element );
 config.connections
 	.filter( cxn => cxn.type == "noir-contrib-irc" )
 	.forEach( cxn => {
-		var config = cxn.config || {};
+		var config = {};
+		var channels = cxn.channels || [];
+		if ( cxn.config ) {
+			Object.keys( cxn.config ).forEach( key => {
+				config[ key ] = cxn.config[ key ];
+			});
+			config.channels = cxn.channels;
+		}
 		if ( config.channels ) {
-			cxn.channels = config.channels;
+			channels = config.channels;
 			delete config["channels"];
 		}
 		if ( ! config.userName ) {
@@ -147,8 +112,16 @@ config.connections
 		if ( ! config.nick ) {
 			config.nick = cxn.userName;
 		}
-		cxn.config = config;
-		var irc = new NoirContribIrc( cxn.host, cxn.name || cxn.host, cxn.userName, cxn.config, cxn.channels, chatAreaFactory, tabset );
+		var irc = new NoirContribIrc(
+			cxn.host,
+			cxn.name || cxn.host,
+			cxn.userName,
+			config,
+			channels,
+			chatAreaFactory,
+			tabset
+		);
+
 
 		if ( cxn.displayedMessageTransforms ) {
 			cxn.displayedMessageTransforms.forEach( name => {
@@ -187,32 +160,4 @@ config.connections
 				}
 			});
 		}
-
-		// irc.displayedMessageTransforms.push( function strToMarkdown( str ) {
-		// 	return marked( str, {
-		// 		gfm: true,
-		// 		breaks: true,
-		// 		sanitize: true,
-		// 		smartypants: true
-		// 	});
-		// });
-
-
-
-		// irc.displayedMessageTransforms.push( function strAddYouTube( str ) {
-		// 	return youTubeIframe.transformString( str );
-		// });
-
-		// irc.displayedMessageTransforms.push( function strAddLinks( str ) {
-		// 	return linkifyHtml( str );
-		// });
-
-		// irc.displayedMessageTransforms.push( function getLinkImage( str ) {
-		// 	return imgPreview.transformString( str );
-		// });
-
-		// irc.sentMessageTransforms.push( function strInjectEmoji( str ) {
-		// 	return emojione.shortnameToUnicode( str );
-		// });
-
 	});
